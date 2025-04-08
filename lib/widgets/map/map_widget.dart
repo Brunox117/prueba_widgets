8ba2ced08d6c326b10df2ea239dfaca280035094
Bruno Jimenez <omarjm0910@gmail.com>
2025-04-08 12:31:44 -0600

    modificaciones en el mapa

--- lib/widgets/map/map_widget.dart
+++ lib/widgets/map/map_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prueba_widgets/widgets/shared/custom_card.dart';
 import 'package:flutter_map/flutter_map.dart';
+// ignore: depend_on_referenced_packages
 import 'package:latlong2/latlong.dart';
 
 class MapWidget extends StatelessWidget {
@@ -8,21 +9,20 @@ class MapWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textStyles = Theme.of(context).textTheme;
     return CustomCard(
-        child: Container(
+        child: SizedBox(
       height: MediaQuery.of(context).size.height * 0.8,
       width: double.infinity,
       child: Column(
         children: [
           const Text(
-            'Mapa',
+            'Ubicación del usuario',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
           Expanded(
             child: FlutterMap(
               options: const MapOptions(
-                initialCenter: LatLng(19.4326, -99.1332), // Ciudad de México
+                initialCenter: LatLng(19.4326, -99.1332), 
                 initialZoom: 13.0,
               ),
               children: [
