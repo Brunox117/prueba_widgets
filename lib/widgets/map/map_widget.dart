100474d5d82b225704d84e02d30d79c0cd1ac6c7
Bruno Jimenez <omarjm0910@gmail.com>
2025-04-08 12:30:26 -0600

    Implementacion basica del mapa

--- lib/widgets/map/map_widget.dart
+++ lib/widgets/map/map_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prueba_widgets/widgets/shared/custom_card.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapWidget extends StatelessWidget {
   const MapWidget({super.key});
@@ -16,6 +18,20 @@ class MapWidget extends StatelessWidget {
           const Text(
             'Mapa',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: FlutterMap(
+              options: const MapOptions(
+                initialCenter: LatLng(19.4326, -99.1332), // Ciudad de México
+                initialZoom: 13.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+            ),
           ),
         ],
       ),
